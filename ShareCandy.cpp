06abac59f61bfc57599dab9c2ba711de9b444911
Alice_Mars <yaogongtonghua@foxmail.com>
2020-04-30 13:08:30 +0800

    分糖果

--- ShareCandy.cpp
+++ ShareCandy.cpp
@@ -1,54 +1,49 @@
-#include<iostream>
-using namespace std;
-bool EqualCandy(int a[],int n)
-{
-	if(n <2 || a==NULL ||n>100)
-	return false;
-	int i;
-	bool equal=true;
-	for(i=1;i<=n;++i)
-	{
-		cout<<a[i]<<" ";
-		if(a[1]!=a[i])
-		 {
-		 	equal=false;
-		 //	break;
-		 }
-	}
-	cout<<endl;
-	return equal;
-}
+#include<stdio.h>
+int a[1003];
+int n;
+int check();
+int answer = 0;
 int main()
 {
-	int N,i;
-	int a[101];
-	cin >> N;
-	for(i=1;i<=N;++i)
+	scanf("%d",&n);
+	int i;
+	for(i=0 ; i<n ; i++)
+	scanf("%d",&a[i]);
+	int bingo = 0;
+	while(!check())
 	{
-		cin >> a[i];
-	}
-	int cnt=0;
-	i=1;
-	while(!EqualCandy(a,N))
-	{	
-		if(a[i]&1)
+		int t = a[0];
+		for( i=0 ; i<n-1 ; i++)
 		{
-			a[i]=a[i]+1;
-			cnt++;
-			cout<< "*****";
-		}	
-		a[i]=a[i]/2;
-		
-		if(i==1)
-		 	a[3]=a[3]+a[i];
-		else{
-			a[i-1]=a[i-1]+a[i];	
-		}	
-		if(i%N==0)
-		{		
-			i=0;
+			a[i] = a[i]/2 + a[i+1]/2;
+			if(a[i]%2) 
+			{
+				a[i]++;
+				answer++;
+			}
 		}
-		++i;
+		a[n-1] = a[n-1]/2 +t/2;
+		if(a[n-1]%2)
+		{
+			a[n-1]++;
+			answer++;
+		}
 	}
-	cout << cnt <<endl;
+	printf("%d\n",answer);
+	return 0;
 }
+int check()
+{
+	int i;
+	for( i=0 ; i<n-1 ; i++)
+	{
+		if(a[i]==a[i+1]) continue;
+		return 0;
+	}
+	if(a[n-1]==a[0]) return 1;
+	return 0;
+}
+ 
+ 
+ 
+ 
