00945ce538f6ace043570636777ffe7e03fd6757
Alice_Mars <yaogongtonghua@foxmail.com>
2020-05-05 21:19:11 +0800

    error

---
+++ practice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i;
+     //  for(i=0;i<num)
+
+    }
+};
+int main()
+{
+	int nums[]={2,7,11,15};
+	int x=9;
+	Solution *S=new Solution();
+	S->twoSum(nums,x);
+}
